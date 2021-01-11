03a68c7c24712303dd25a482cf870b1f65702a4f
Alexander Olshanskiy <sander2411ukr@gmail.com>
2021-01-11 17:55:40 +0200

    chnage api

--- src/_REST/API.ts
+++ src/_REST/API.ts
@@ -101,7 +101,7 @@ const deleteSPDataItem = (listGuid: string, dataItemID: number) =>
     .delete()
     .then(() => dataItemID);
 
-export const API_: API = {
+export const API: API = {
   agenda: {
     getData: async () =>
       getSPData<QueryAppointmentDataItem>(GuidList.Appointment, SelectFields.Appointment, OrderBy.Appointment, FilterItems.Appointments),
@@ -159,7 +159,7 @@ export const API_: API = {
   },
 };
 
-export const API: API = {
+export const API_: API = {
   agenda: {
     getData: () => fetch(`${ROOT_URL}/appointments`).then((response) => response.json()),
 
